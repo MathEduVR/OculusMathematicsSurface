09c0d24859d31d140a5e9013f2713316acdbb022
Kazushi Ahara <ahara@meiji.ac.jp>
2019-11-02 10:48:35 +0900

    Caption
    レイを当てた時だけタイトルが出現するようにした。

--- OculusMathematicsSurfaces/Assets/Scripts/RightHandRay.cs
+++ OculusMathematicsSurfaces/Assets/Scripts/RightHandRay.cs
@@ -23,10 +23,13 @@ public class RightHandRay : MonoBehaviour
     public Collider col;
     public Vector3 eyeHeight = Vector3.up;
 
+    TextMesh[] allCaptions;
+
     // Start is called before the first frame update
     void Start()
     {
         StartRoutine();
+        allCaptions = FindObjectsOfType<TextMesh>();
     }
     // Update is called once per frame
     void Update()
@@ -116,12 +119,22 @@ public class RightHandRay : MonoBehaviour
         }
         if (col != null && col.name.Contains("box_"))
         {
+            if(allCaptions!=null)
+                for (int i = 0; i < allCaptions.Length; i++)
+                    allCaptions[i].gameObject.transform.localPosition = new Vector3(0f, -10f, 0f);
+            GameObject obj = col.gameObject;
+            TextMesh childText = obj.GetComponentInChildren<TextMesh>();
+            //Debug.Log(childText);
+            if (childText != null)
+            {
+                childText.gameObject.transform.localPosition = new Vector3(0f, 0.5f, 0);
+            }
             GetComponent<MeshRenderer>().material = CursorOnMaterial;
             if (OVRInput.Get(OVRInput.Button.One) 
                 || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) 
                 || OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
             {
-                GameObject obj = col.gameObject;
+                //GameObject colObj = col.gameObject;
                 Debug.Log(obj.name);
                 if (obj.name.Contains("box_Moebius"))
                 {
